8dd84c48cb69c8cf8879645dec4ab158c7577348
Muhammad Zulfikri <zulfikri@nawatech.co>
2025-03-01 23:22:44 +0700

    Add images to Hall of Fame and update modal functionality for enhanced user interaction

--- js/main.js
+++ js/main.js
@@ -6,63 +6,124 @@ if (hofList) {
     {
       name: "Ade Chandra",
       image: "./assets/images/hall-of-fame/ade-chandra.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - ADE CHANDRA.jpg",
     },
     {
       name: "Christian Hadinata",
       image: "./assets/images/hall-of-fame/christian-hadinata.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - CHRISTIAN HADINATA.jpg",
     },
     {
-      name: "Eddy Kurniawan",
-      image: "./assets/images/hall-of-fame/eddy-kurniawan.png",
+      name: "Alan Budi Kusuma",
+      image: "./assets/images/hall-of-fame/alan-budi-kusuma.jpg",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - ALAN BUDI KUSUMA.jpg",
+    },
+    {
+      name: "Susy Susanti",
+      image: "./assets/images/hall-of-fame/susy-susanti.jpg",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - SUSY SUSANTI.jpg",
     },
     {
       name: "Icuk Sugiarto",
       image: "./assets/images/hall-of-fame/icuk-sugiarto.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - ICUK SUGIARTO.jpg",
     },
     {
       name: "Imelda Wiguna",
       image: "./assets/images/hall-of-fame/imelda-wiguna.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - IMELDA WIGUNA.jpg",
     },
     {
       name: "Kartono H. Atmanto",
       image: "./assets/images/hall-of-fame/kartono-h-atmanto.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - KARTONO H. ATMANTO.jpg",
     },
     {
       name: "Liem Swie King",
       image: "./assets/images/hall-of-fame/liem-swie-king.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - LIEM SWIE KING.jpg",
     },
     {
       name: "Liliyana Natsir",
       image: "./assets/images/hall-of-fame/liliyana-natsir.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - LILIYANA NATSIR.jpg",
     },
     {
       name: "Rudy Hartono",
       image: "./assets/images/hall-of-fame/rudy-hartono.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - RUDY HARTONO.jpg",
     },
     {
       name: "Rudy Heryanto",
       image: "./assets/images/hall-of-fame/rudy-heryanto.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - RUDY HERYANTO.jpg",
     },
     {
       name: "Tan Joe Hok",
       image: "./assets/images/hall-of-fame/tan-joe-hok.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - TAN JOE HOK.jpg",
     },
     {
       name: "Tantowi Ahmad",
       image: "./assets/images/hall-of-fame/tantowi-ahmad.png",
+      popupImage:
+        "./assets/images/hall-of-fame/pop-up/POP UP - TANTOWI AHMAD.jpg",
     },
   ];
+
+  // Create modal container
+  const modalContainer = document.createElement("div");
+  modalContainer.innerHTML = `
+    <div class="modal fade" id="hofModal" tabindex="-1" aria-labelledby="hofModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+          <div class="modal-body text-center position-relative p-0">
+            <button type="button" class="btn btn-link p-4 text-white position-absolute top-0 end-0" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-circle-xmark fa-xl"></i></button>
+            <img src="" alt="" class="img-fluid rounded-2" id="hofModalImage">
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  document.body.appendChild(modalContainer);
+
   // Render the hall of fame list
   hofData.forEach((data) => {
     const hofItem = document.createElement("div");
     hofItem.classList.add("col");
     const hofItemFrame = document.createElement("div");
-    hofItemFrame.classList.add("ratio");
-    hofItemFrame.classList.add("ratio-1x1");
+    hofItemFrame.classList.add("hof-frame", "ratio", "ratio-1x1");
+    hofItemFrame.style.cursor = "pointer";
+
+    // Add click event listener
+    hofItemFrame.addEventListener("click", () => {
+      const modal = new bootstrap.Modal(document.getElementById("hofModal"));
+      // const modalTitle = document.getElementById("hofModalLabel");
+      const modalImage = document.getElementById("hofModalImage");
+
+      // modalTitle.textContent = data.name;
+      modalImage.src = data.popupImage;
+      modalImage.alt = data.name;
+
+      modal.show();
+    });
+
     hofItem.appendChild(hofItemFrame);
     hofItemFrame.innerHTML = `
-          <img class="object-fit-cover" src="${data.image}" alt="${data.name}">
-      `;
+      <img class="object-fit-cover" src="${data.image}" alt="${data.name}">
+    `;
     hofList.appendChild(hofItem);
   });
 }
